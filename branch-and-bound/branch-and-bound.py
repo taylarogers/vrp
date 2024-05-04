b40631f2564e51dd3997f8ff091293bc54ef4c39
taylarogers <taylarogers02@gmail.com>
2024-05-04 16:27:28 +0200

    Update of input

--- branch-and-bound/branch-and-bound.py
+++ branch-and-bound/branch-and-bound.py
@@ -1,8 +1,7 @@
 # Code for branch and bound
 
 class Node:
-    def __init__(self, location):
-        self.location = location
+    def __init__(self):
         self.visited = False
 
 def main():
@@ -10,15 +9,11 @@ def main():
     num_customers = int(input("Enter the number of customers: "))
     num_vehicles = int(input("Enter the number of vehicles: "))
 
-    # Locations of each customer
-    nodes = []
-    for i in range(num_customers):
-        x, y = map(int, input(f"Location (x y) of customer {i+1}: ").split())
-        nodes.append(Node((x, y)))
-
-    # Add depot to end of list for separation of nodes
-    depot = Node((0, 0))
-    nodes.append(depot)
+    # Pairwise distances between each customer
+    distances = []
+    for i in range(num_customers + 1):
+        distances_row = list(map(int, input(f"Enter the distances from node {i} to all other nodes separated by space: ").split()))
+        distances.append(distances_row)
 
 if __name__ == "__main__":
     main()
